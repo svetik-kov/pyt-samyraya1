d8f3dc56726926315f3087bb881c2397c0569836
Svetlana_Kovalevich <svetik14-06@mail.ru>
2022-07-06 19:05:46 +0300

    31 урок добавление useRef

--- src/component/Profile/MyPosrs/Post/MyPosts.tsx
+++ src/component/Profile/MyPosrs/Post/MyPosts.tsx
@@ -1,5 +1,5 @@
 import s from "./MyPosts.module.css";
-import React from "react";
+import React, {useRef} from "react";
 import {Post} from "../Post";
 /*import {PostsDataType} from "../../../../App";*/
 import {RootStateType} from "../../../../redux/State";
@@ -16,13 +16,25 @@ export const MyPosts = (props:postsDataPropsType) => {
         {id: 2, message: 'It is my first post', likesCount: 30},
     ]*/
     let postsElements= props.state.ProfilePage.posts.map((p)=>   < Post key={p.id} message={p.message} likesCount={p.likesCount}/>);
+
+    let newPostElement=useRef<HTMLTextAreaElement>(null);
+    let addPost=()=>{
+       if (newPostElement.current!==null){
+           alert(newPostElement.current.value)
+       }
+    }
+
+
+
     return (
         <div className={s.postBlock}>
             <h3>MY posts</h3>
             <div>
-                <div ><textarea></textarea></div>
+                <div >
+                    <textarea ref={newPostElement}></textarea>
+                </div>
                 <div>
-                    <button> Add post</button>
+                    <button onClick={addPost}> Add post</button>
                 </div>
             </div>
 
