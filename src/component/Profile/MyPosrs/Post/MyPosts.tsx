eeb492156203db11292ea3c7acec79481936ebe8
Svetlana_Kovalevich <svetik14-06@mail.ru>
2022-08-04 14:34:11 +0300

    39 урок action type, action creator

--- src/component/Profile/MyPosrs/Post/MyPosts.tsx
+++ src/component/Profile/MyPosrs/Post/MyPosts.tsx
@@ -1,44 +1,51 @@
 import s from "./MyPosts.module.css";
 import React, {ChangeEvent, useRef} from "react";
 import {Post} from "../Post";
-/*import {PostsDataType} from "../../../../App";*/
-import {ActionTypes, RootStateType} from "../../../../redux/State";
-import {message} from "antd";
+
+import {
+    ActionTypes,
+    RootStateType,
+  } from "../../../../redux/State";
+
 
 export type  postsDataPropsType = {
-    /*postsData:Array<PostsDataType>*/
+
     state: RootStateType
-    dispatch:(action:ActionTypes)=>void
-   /* addPosts: (postMessage: string) => void
-    updateNewPost: (newText: string) => void*/
+    dispatch: (action: ActionTypes) => void
     message: string
 }
 
-export const MyPosts = (props: postsDataPropsType) => {
-    debugger
 
+export const MyPosts = (props: postsDataPropsType) => {
+
+    const addPostActionCreator = () => {
+        return {
+            type: 'ADD-POSTS',
+            postMessage: props.message
+        } as const
+    }
+
+    let updateNewPostTextActionCreator = (value:string) => {
+        return {
+            type: 'UPDATE-NEW-POSTS',
+            newText: value
+        } as const
+    }
 
     let postsElements = props.state.ProfilePage.posts.map((p) => < Post key={p.id} message={p.message}
                                                                         likesCount={p.likesCount}/>);
 
-    /*  let newPostElement=useRef<HTMLTextAreaElement>(null);
-     let addPost=()=>{
-        if (newPostElement.current!==null){
-           props.addPosts(newPostElement.current.value)
-        }
-     }*/
 
-    /* let newPostElement=React.createRef<HTMLTextAreaElement>();*/
     let addPost = () => {
-        props.dispatch({type:'ADD-POSTS',postMessage:props.message})
-        props.dispatch({type:'UPDATE-NEW-POSTS',newText:''})
+        props.dispatch(addPostActionCreator())
+        props.dispatch(updateNewPostTextActionCreator(''))
 
 
     }
 
     const onPostChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-               /* props.updateNewPost(e.currentTarget.value)*/
-        props.dispatch({type:'UPDATE-NEW-POSTS',newText:e.currentTarget.value})
+
+        props.dispatch(updateNewPostTextActionCreator(e.currentTarget.value))
     }
 
     return (
@@ -46,7 +53,7 @@ export const MyPosts = (props: postsDataPropsType) => {
             <h3>MY posts</h3>
             <div>
                 <div>
-                    <textarea onChange={onPostChange}  value={props.message}/>
+                    <textarea onChange={onPostChange} value={props.message}/>
                 </div>
                 <div>
                     <button onClick={addPost}> Add post</button>
@@ -55,8 +62,6 @@ export const MyPosts = (props: postsDataPropsType) => {
 
             <div className={s.posts}>
                 {postsElements}
-                {/*<Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
-                <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>*/}
             </div>
         </div>
 
--- src/redux/State.tsx
+++ src/redux/State.tsx
@@ -30,26 +30,26 @@ export type RootStateType = {
 /*let _callSubscriber=()=>{
     console.log('hello my friend')
 }*/
-type AddPostsActionType={
-    type:'ADD-POSTS'
-    postMessage:string
+type AddPostsActionType = {
+    type: 'ADD-POSTS'
+    postMessage: string
 }
-type updateNewPostActionType={
-    type:'UPDATE-NEW-POSTS'
-    newText:string
+type updateNewPostActionType = {
+    type: 'UPDATE-NEW-POSTS'
+    newText: string
 }
 
-export type ActionTypes=AddPostsActionType|updateNewPostActionType
-export type storeType={
-    _state:RootStateType
-    _callSubscriber: ()=>void
-   /* addPosts: (newMessage: string)=>void
-    updateNewPost :  (newText: string)=>void*/
-    subscribe : (callBack: () => void)=>void
-    getState:()=>RootStateType
-    dispatch:(action:ActionTypes)=>void
+export type ActionTypes = AddPostsActionType | updateNewPostActionType
+export type storeType = {
+    _state: RootStateType
+    _callSubscriber: () => void
+    /* addPosts: (newMessage: string)=>void
+     updateNewPost :  (newText: string)=>void*/
+    subscribe: (callBack: () => void) => void
+    getState: () => RootStateType
+    dispatch: (action: ActionTypes) => void
 }
-export let store:storeType = {
+export let store: storeType = {
     _state: {
         ProfilePage: {
             posts: [
@@ -88,86 +88,36 @@ export let store:storeType = {
     subscribe(callBack: () => void) {
         this._callSubscriber = callBack
     },
-   /* addPosts(postMessage: string) {
 
-        const newPost: PostType = {
-            id: new Date().getTime(),
-            message: postMessage,
-            likesCount: 103
+    dispatch(action) {
+        if (action.type === 'ADD-POSTS') {
+            const newPost: PostType = {
+                id: new Date().getTime(),
+                message: action.postMessage,
+                likesCount: 103
+            }
+            this._state.ProfilePage.posts.push(newPost)
+            this._callSubscriber()
+        } else if (action.type === 'UPDATE-NEW-POSTS') {
+            this._state.ProfilePage.newText = action.newText
+            this._callSubscriber()
         }
-        this._state.ProfilePage.posts.push(newPost)
-        this._callSubscriber()
-    },
-    updateNewPost(newText: string) {
-
-        this._state.ProfilePage.newText = newText
-        this._callSubscriber()
-    },*/
-    dispatch(action){
-if (action.type==='ADD-POSTS'){
-    const newPost: PostType = {
-        id: new Date().getTime(),
-        message: action.postMessage,
-        likesCount: 103
-    }
-    this._state.ProfilePage.posts.push(newPost)
-    this._callSubscriber()
-}else
-    if (action.type==='UPDATE-NEW-POSTS'){
-        this._state.ProfilePage.newText = action.newText
-        this._callSubscriber()
-    }
 
     }
 }
-/*let state: RootStateType = {
-    ProfilePage: {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: 14},
-            {id: 2, message: 'It is my first post', likesCount: 30},
-            {id: 3, message: 'It is my second post', likesCount: 103}
-        ],
 
-        dialogs: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Sveta'},
-            {id: 3, name: 'Maksim'},
-            {id: 4, name: 'Anna'},
-            {id: 5, name: 'Helena'},
-            {id: 6, name: 'Maksim'},
-        ],
-        newText:'',
-    },
-    MessagePage: {
-        messages: [
-            {id: 1, message: 'Hello'},
-            {id: 2, message: 'How are you?'},
-            {id: 3, message: 'Hi!'},
-            {id: 4, message: 'Yo!'},
-            {id: 5, message: 'Yo!!!'},
-        ]
-    },
-    Sidebar:{}
+/* export const addPostActionCreator = () => {
+    return {
+        type: 'ADD-POSTS',
+        postMessage: props.message
+    } as const
+}
+
+export const updateNewPostTextActionCreator = (value:string) => {
+    return {
+        type: 'UPDATE-NEW-POSTS',
+        newText: value
+    } as const
 }*/
-
-/*export const addPosts=(postMessage:string)=>{
-
-    const newPost: PostType={
-        id: new Date().getTime(),
-        message: postMessage,
-        likesCount: 103
-    }
-    state.ProfilePage.posts.push(newPost)
-    rerenderEntireTree()
-}
-export const updateNewPost=(newText:string)=>{
-
-    state.ProfilePage.newText=newText
-    rerenderEntireTree()
-}
-export const subscribe=(callBack:()=>void)=>{
-    rerenderEntireTree=callBack
-}*/
-
 
 export default store
